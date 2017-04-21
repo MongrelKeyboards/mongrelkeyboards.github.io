6c11d500f50fc9d3167f3ef70c0a98eb46252a1f
Callan Eckett <callaneckett.system@gmail.com>
2017-04-21 16:35:06 +0100

    Added jumbotron and project templates

---
+++ js/website.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+     
+    $(".home-carousel").owlCarousel({
+     
+        autoPlay: 10000, //Set AutoPlay to 3 seconds
+     
+        autoHeight : true,
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+     
+    });
+     
+});
+
